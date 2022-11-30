36f45142efafb716739a6bee3d3568d9fdaaa006
Ian van der Linde <ivdl@sun.ac.za>
2022-11-30 11:37:52 +0200

    Amend table DDL to skip fields removed in the fall 2022 version.
    Removes APPLN_NR_EPODOC and EVENT_IMPACT fields from 201 and 803
    respectively.

--- create_patstat_tables.sql
+++ create_patstat_tables.sql
@@ -5,7 +5,6 @@ CREATE TABLE tls201_appln (
     appln_kind char(2) DEFAULT '' NOT NULL,
     appln_filing_date date DEFAULT '9999-12-31' NOT NULL,
     appln_filing_year smallint DEFAULT '9999' NOT NULL,
-    appln_nr_epodoc varchar(20) DEFAULT '' NOT NULL,
     appln_nr_original varchar(100) DEFAULT '' NOT NULL,
     ipr_type char(2) DEFAULT '' NOT NULL,
     receiving_office char(2) DEFAULT '' NOT NULL,
@@ -292,7 +291,6 @@ CREATE TABLE tls801_country (
 CREATE TABLE tls803_legal_event_code (
     event_auth char(2) DEFAULT '' NOT NULL,
     event_code varchar(4) DEFAULT '' NOT NULL,
-    event_impact char(1) DEFAULT '',
     event_descr varchar(250) DEFAULT '',
     event_descr_orig varchar(250) DEFAULT '',
     event_category_code char(1) DEFAULT '',
